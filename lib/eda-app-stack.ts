0cc0da3b0d284b42937b92f32d076152742d2ceb
ayoog <20098135@mail.wit.ie>
2025-04-02 14:41:04 +0100

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -31,8 +31,10 @@ export class EDAAppStack extends cdk.Stack {
       const newImageTopic = new sns.Topic(this, "NewImageTopic", {
         displayName: "New Image topic",
       }); 
-  
-  
+
+      const mailerQ = new sqs.Queue(this, "mailer-queue", {
+        receiveMessageWaitTime: cdk.Duration.seconds(10),
+      });
 
   // Lambda functions
 
@@ -47,6 +49,14 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
+
   // S3 --> SQS
   imagesBucket.addEventNotification(
     s3.EventType.OBJECT_CREATED,
@@ -57,6 +67,9 @@ export class EDAAppStack extends cdk.Stack {
     new subs.SqsSubscription(imageProcessQueue)
 );
 
+newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+
 
 
  // SQS --> Lambda
@@ -66,6 +79,27 @@ export class EDAAppStack extends cdk.Stack {
   });
 
   processImageFn.addEventSource(newImageEventSource);
+
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(5),
+  }); 
+
+  mailerFn.addEventSource(newImageMailEventSource);
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
+
+
 
   // Permissions
 
